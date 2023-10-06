6cbe18e22c5a234ad492fd4d36104ec61650c713
Sihoiba <dominic.decesare+github@gmail.com>
2023-10-06 20:32:56 +0100

    Removed commented out code, and reduced logging

--- main.lua
+++ main.lua
@@ -27,59 +27,12 @@ function mod.run_store_equipment_ui( self, entity, return_entity )
     ui:terminal( entity, nil, list )
 end
 
--- function mod.run_store_item_ui( self, entity, return_entity )
-    -- local list          = {}    
-    -- local max_len       = 1
-    -- local postbag_entity = entity:child( "hidden_entity_postbag" )
-    
-    -- nova.log("get data")
-    -- for k,v in pairs(entity.data) do
-        -- nova.log(k)
-    -- end
-    -- nova.log("get attribute")
-    -- for k,v in pairs(entity.attribute) do
-        -- nova.log(k)
-    -- end
-    -- nova.log("get child")
-    -- for k,v in pairs(entity.child) do
-        -- nova.log(k)
-    -- end
-    
-    -- for c in entity:children() do
-        -- if c:get_name() then     
-            -- nova.log("Child "..c:get_name())
-        -- end  
-        -- if c.flags and c.flags.data[ EF_ITEM ] and not c.slot and not c.weapon then
-            -- nova.log("Item "..c:get_name())
-            -- max_len = math.max( max_len, string.len( c:get_name() ) )
-            -- table.insert( list, {
-                        -- name = c:get_name(),
-                        -- target = self,
-                        -- parameter = c,
-                        -- confirm = true,
-            -- })                                  
-        -- end       
-    -- end
-    
-    -- table.insert( list, {
-        -- name = ui:text("ui.lua.common.cancel"),
-        -- target = return_entity or self,
-        -- cancel = true,
-    -- })
-    -- list.title = (postbag_entity.data.max_space - postbag_entity.data.used_space).." spaces remaining."
-    -- list.size  = coord( math.max( 30, max_len + 6 ), 0 )
-    -- list.confirm = "Are you sure you want to send this item?"
-    -- ui:terminal( entity, nil, list )
--- end
-
 function mod.run_retrieve_ui( self, entity, return_entity )
     local list          = {}        
     local max_len       = 1
     local postbag_entity = entity:child( "hidden_entity_postbag" )
 
-	nova.log("Checking postbag")
     for c in postbag_entity:children() do
-		nova.log("postbag has children")
         if c.flags and c.flags.data[ EF_ITEM ] then
             nova.log(c:get_name())
             max_len = math.max( max_len, string.len( c:get_name() ) )
@@ -143,46 +96,6 @@ register_blueprint "terminal_send_equipment"
     }, 
 }
 
--- register_blueprint "terminal_send_item"
--- {
-    -- text = {
-        -- entry = "Send item",
-        -- desc  = "Send an item to retrieve at a later station. Max 3 sent and awaiting collection.",
-    -- },
-    -- data = {
-        -- terminal = {
-            -- priority = 76,
-        -- },
-    -- },
-    -- callbacks = {
-        -- on_activate = [=[
-            -- function( self, who, level, param )
-                -- local postbag = who:child( "hidden_entity_postbag" )
-                -- if not param then
-                    -- if postbag and postbag.data and postbag.data.used_space == postbag.data.max_space then
-                        -- ui:set_hint( "Maximum items awaiting collection", 50, 1 )
-                    -- else
-                        -- local parent = self:parent()
-                        -- world:play_sound( "ui_terminal_accept", parent )
-                        -- mod.run_store_item_ui( self, who, parent )
-                        -- return -1                       
-                    -- end
-                -- else                                
-                    -- if postbag and postbag.data and postbag.data.used_space ~= postbag.data.max_space then
-                        -- nova.log("storing "..param.text.name)                        
-                        -- level:drop_item( who, param )
-                        -- level:hard_place_entity( param, ivec2( 0,0 ) )
-                        -- level:pickup_drop( postbag, param, false )
-                        -- postbag.data.used_space = postbag.data.used_space + 1   
-                    -- end 
-                
-                    -- return 100
-                -- end
-            -- end 
-        -- ]=]
-    -- }, 
--- }
-
 register_blueprint "station_retrieve_equipment"
 {
     text = {
@@ -240,32 +153,24 @@ register_blueprint "hidden_entity_postbag"
 
 postal_service = {}
 function postal_service.on_entity( entity )	
-	nova.log("postal_service.on_entity start")
     if entity.data and entity.data.ai and entity.data.ai.group == "player" then
-		nova.log("Attaching postbag to player")
         entity:attach( "hidden_entity_postbag" )
     end 
     if (world:get_id(entity) == "terminal" or world:get_id(entity) == "trial_arena_terminal") then
-		nova.log("Attaching send to terminal")
         entity:attach( "terminal_send_equipment" )
     end
     if (world:get_id(entity) == "medical_station") then
-		nova.log("Attaching retrieve medical")
         entity:attach( "station_retrieve_equipment" )
     end
 	if (world:get_id(entity) == "terminal_ammo") then
-		nova.log("Attaching retrieve ammo")
         entity:attach( "station_retrieve_equipment" )
     end
 	if (world:get_id(entity) == "manufacture_station") then
-		nova.log("Attaching retrieve manufacture")
         entity:attach( "station_retrieve_equipment" )
     end
 	if (world:get_id(entity) == "technical_station") then
-		nova.log("Attaching retrieve technical")
         entity:attach( "station_retrieve_equipment" )
     end
-	nova.log("postal_service.on_entity end")
 end
 
 world.register_on_entity( postal_service.on_entity )
